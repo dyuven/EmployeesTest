17f64862b59be22acff19c818af5db29225fd172
Дю Владимир Андреевич <mr.dyuven@mail.ru>
2022-08-16 22:37:14 +0300

    Initial commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Вова Дю on 16.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
